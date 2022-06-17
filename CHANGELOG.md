26bda04b85593d1dc285f3be4ba7daa96e5ab6e9
Joseph Stoops <Joseph.Stoops@FamilySearch.org>
2022-06-17 12:24:08 -0600

    Set up Webpack to use Babel to support modern JS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
-## [1.0.7] - 2022-06-17
+## [1.0.12] - 2022-06-17
+
+- Set up webpack to use Babel to support modern JS features like nullish coallescing.
+
+## [1.0.11] - 2022-06-17
 
 - Set up Webpack to bundle package.
-  - Reduced package file from 59.4kB down to 17.0kB.
+  - Reduced package file from 59.4kB down to 27.4kB.
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-env' ],
+            plugins: [ '@babel/plugin-proposal-object-rest-spread' ],
+          },
+        },
+      },
+    ],
+  },
 }
