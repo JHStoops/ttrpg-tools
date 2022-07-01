c679d59b82f83eb137f9615445b883e4cd05eff1
Joseph Stoops <Joseph.Stoops@FamilySearch.org>
2022-07-01 10:47:39 -0600

    Add codeclimate ESLint file

---
+++ .codeclimate.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'jsx-a11y/no-noninteractive-tabindex': 'warn',
+    'no-template-curly-in-string': 'warn'
+  }
+}
