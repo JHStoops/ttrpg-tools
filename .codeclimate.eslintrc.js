e8ecd7d7ed38d9f74bf234a8d581c5889b4bb99e
Joseph Stoops <Joseph.Stoops@FamilySearch.org>
2022-07-01 10:49:57 -0600

    Change CC ESLint file from js to json

--- .codeclimate.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-named-as-default': 'off',
-    'jsx-a11y/no-noninteractive-tabindex': 'warn',
-    'no-template-curly-in-string': 'warn'
-  }
-}
