e884d3a59e17d0bfc6c4fdce1999514c27ff2779
Joseph Stoops <joseph.h.stoops@gmail.com>
2024-01-29 14:34:51 -0700

    Add subclasses to NPCs

--- src/generators/npc.js
+++ src/generators/npc.js
@@ -8,6 +8,7 @@ import { generateNpcName, generateTownName } from './names.js'
  * @param {String} homeTown - Specify the hometown of the character.
  * @param {Array} languages - Specify the languages spoken by the character.
  * @param {String} npcClass - Specify the class of the character (barbarian/cleric/etc).
+ * @param {String} npcSubclass - Specify the subclass of the character (Alchemist, College of Valor, Life Domain, etc).
  * @param {String} occupation - Specify the occupation of the character (adventurer/merchant/etc).
  * @param {Boolean} randomizeClass - Whether to randomize the NPC's class. Guarantees NPC has a class, otherwise NPC only has a small chance of having a class.
  * @param {String} race - Specify the race of the character (Orc/Human/etc).
@@ -15,7 +16,7 @@ import { generateNpcName, generateTownName } from './names.js'
  * @returns {Object} - a randomized NPC
  */
 export function generateNpc({
-  familyName, givenName, homeTown, languages, npcClass, occupation, race, randomizeClass, sex,
+  familyName, givenName, homeTown, languages, npcClass, npcSubclass, occupation, race, randomizeClass, sex,
 } = {}) {
   if (![ undefined, 'female', 'male' ].includes(sex)) throw Error('generateNpc(): sex paramter must be either "female", "male", or undefined.')
   const npcSex = [ 'female', 'male' ].includes(sex) ? sex : getRandomElement([ 'female', 'male' ])
@@ -25,12 +26,15 @@ export function generateNpc({
 
   let npcClassFinal = npcClass ?? ''
   if (randomizeClass || (!npcClass && Math.random() > 0.9)) npcClassFinal = getRandomElement(data.classes.get()) // Unless a class is specified, 90% of people are average Joe's with zero class
+  let npmSubclassFinal = npcSubclass ?? ''
+  if (npcClassFinal) npmSubclassFinal = getRandomElement(data.subclasses.get(npcClassFinal))
   const npcGivenName = givenName ?? generateNpcName(npcSex).givenName
   const npcFamilyName = familyName ?? generateNpcName(npcSex).familyName
   const name = { givenName: npcGivenName, familyName: npcFamilyName, fullName: `${npcGivenName} ${npcFamilyName}` }
 
   return {
     class: npcClassFinal,
+    subclass: npmSubclassFinal,
     homeTown: homeTown ?? generateTownName(),
     languages: languages ?? [ ...new Set([ 'Common', getRandomElement(data.languages.get()) ]) ], // Speak Common and up to one other language
     ...name,
