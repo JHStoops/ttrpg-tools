e9f43f4d1a5380e5188c847dad9e8605f58a0065
Joseph Stoops <joseph.h.stoops@gmail.com>
2024-01-29 14:35:20 -0700

    Implement `getRandomStandardDev` in utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,3 +9,14 @@ export function getRandomElement(collection) {
   if (collection.length === 0) throw Error('getRandomElement(): collection cannot be an empty array.')
   return collection[getRandomInteger(collection.length - 1, 0)]
 }
+
+// Standard Normal variate using Box-Muller transform.
+// Found this Standard Normal variation function here: https://stackoverflow.com/a/36481059
+export function getRandomStandardDev(mean = 0, stdev = 1) {
+  const u = 1 - Math.random() // Converting [0,1) to (0,1]
+  const v = Math.random()
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+
+  // Transform to the desired mean and standard deviation:
+  return z * stdev + mean
+}
